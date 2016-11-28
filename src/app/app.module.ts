654e3cef33222e84e2e5f8a869361cc88e571e76
LeoTM <leo.m@hotmail.co.uk>
2016-11-28 18:07:44 +0000

    Add project code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule  } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }   from './app.component';
+import { UserComponent } from "./user/user.component";
+import { UserDetailComponent } from "./user/user-detail.component";
+import { UserEditComponent } from "./user/user-edit.component";
+import { HomeComponent } from "./home-component.component";
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        UserComponent,
+        UserDetailComponent,
+        UserEditComponent,
+        HomeComponent
+    ],
+    imports: [BrowserModule],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
