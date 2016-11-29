0be87a41bd95356941127bdf172df11676a891d5
LeoTM <leo.m@hotmail.co.uk>
2016-11-29 02:06:38 +0000

    Try router.navigate

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   moduleId: 'module.id',
   selector: 'app-user-component',
   template: `
       <h1>User Component</h1>
       <a [routerLink]="['/user']">User</a>
+      <button (click)="onNavigate()">Go Home</button>
     `
 })
 export class UserComponent {
+  constructor(private router: Router) { }
+  onNavigate() {
+    this.router.navigate(['/']);
+  }
 }
